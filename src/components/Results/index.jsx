4c6751b614e51d205b32edf867b89a5ea5e84d1a
carolina arruda <carolinacalouroarruda@gmail.com>
2023-09-18 17:22:55 +0200

    tweaked css

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -3,23 +3,32 @@ import React, { useState, useEffect } from "react";
 import "./index.scss";
 
 function Results({ data, search }) {
-
   return (
     <div>
       {data.map((character) => (
-         <div key={character.id} className="item-box">
-        <div>
-          <img src={`${character.thumbnail.path}/standard_medium.jpg`} />
+        <div key={character.id} className="item-box">
+          <div>
+            <img src={`${character.thumbnail.path}/standard_medium.jpg`} />
+          </div>
+          <div>
+            <h2>{character.name}</h2>
+            <p>{character.description}</p>
+            <div className="">
+              <span>
+                <strong>#comics: </strong>
+                {character.comics.items.length}{" "}
+              </span>
+              <span>
+                <strong>#series: </strong>
+                {character.series.items.length}{" "}
+              </span>
+              <span>
+                <strong>#stories: </strong>
+                {character.stories.items.length}
+              </span>
+            </div>
+          </div>
         </div>
-        <div>
-          <h2>{character.name}</h2>
-          <p>{character.description}</p>
-          <span>#comics: {character.comics.items.length}</span>
-          <span>#series: {character.series.items.length}</span>
-          <span>#stories: {character.stories.items.length}</span>
-        </div>
-      </div>
-     
       ))}
     </div>
   );
